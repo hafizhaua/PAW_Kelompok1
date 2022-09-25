22f479e65e3ad7165d84e320eb07951ce9ef3974
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2022-09-25 11:31:31 +0700

    feat: add GET all data and filtering method

--- src/app/controllers/donorRequest.controller.js
+++ src/app/controllers/donorRequest.controller.js
@@ -2,7 +2,28 @@ const db = require("../models/index");
 const donorRequest = db.donorRequests;
 
 exports.create = async (req, res) => {};
-exports.readAll = async (req, res) => {};
+
+exports.readAll = async (req, res) => {
+    try {
+        const result = await donorRequest.find();
+        const filters = req.query;
+
+        const filteredResult = result.filter((donorReq) => {
+            let isValid = true;
+            for (key in filters) {
+                isValid = isValid && donorReq[key] == filters[key];
+            }
+            return isValid;
+        });
+
+        res.send(filteredResult);
+    } catch (err) {
+        res.status(500).send({
+            message: err.message || "Error occured on retrieving the requests.",
+        });
+    }
+};
+
 exports.readOne = async (req, res) => {};
 exports.update = async (req, res) => {};
 exports.delete = async (req, res) => {};
