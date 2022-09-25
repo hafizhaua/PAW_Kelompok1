64d9b0d2ac95a121f84cf657999b240b977fa1fe
mraihannn <muhammadraihan@mail.ugm.ac.id>
2022-09-25 12:45:29 +0700

    add READ a data method

--- src/app/controllers/donorRequest.controller.js
+++ src/app/controllers/donorRequest.controller.js
@@ -1,29 +1,39 @@
-const db = require("../models/index");
-const donorRequest = db.donorRequests;
+const db = require("../models/index")
+const donorRequest = db.donorRequests
 
-exports.create = async (req, res) => {};
+exports.create = async (req, res) => {}
 
 exports.readAll = async (req, res) => {
-    try {
-        const result = await donorRequest.find();
-        const filters = req.query;
+  try {
+    const result = await donorRequest.find()
+    const filters = req.query
 
-        const filteredResult = result.filter((donorReq) => {
-            let isValid = true;
-            for (key in filters) {
-                isValid = isValid && donorReq[key] == filters[key];
-            }
-            return isValid;
-        });
+    const filteredResult = result.filter((donorReq) => {
+      let isValid = true
+      for (key in filters) {
+        isValid = isValid && donorReq[key] == filters[key]
+      }
+      return isValid
+    })
 
-        res.send(filteredResult);
-    } catch (err) {
-        res.status(500).send({
-            message: err.message || "Error occured on retrieving the requests.",
-        });
-    }
-};
+    res.send(filteredResult)
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Error occured on retrieving the requests.",
+    })
+  }
+}
 
-exports.readOne = async (req, res) => {};
-exports.update = async (req, res) => {};
-exports.delete = async (req, res) => {};
+exports.readOne = async (req, res) => {
+  try {
+    const id = req.params.id
+    const result = await donorRequest.findById(id)
+    res.send(result)
+  } catch (err) {
+    res.status(409).send({
+      message: err.message || "Error occured when retrieving the request.",
+    })
+  }
+}
+exports.update = async (req, res) => {}
+exports.delete = async (req, res) => {}
