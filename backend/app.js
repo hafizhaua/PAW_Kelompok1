5235a84bcd1b516406dd74dcbd920d5832764c20
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2022-11-17 16:19:05 +0700

    fix: disable auth (temporary)

--- backend/app.js
+++ backend/app.js
@@ -32,8 +32,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/donorRequest", requestRoute);
-app.use("/auth", authRoute);
-app.use("/user", userRoute);
+// app.use("/auth", authRoute);
+// app.use("/user", userRoute);
 
 app.get("*", (req, res) => {
     res.send("This root doesn't exist");
@@ -46,7 +46,7 @@ db.mongoose
     })
     .then(() => {
         console.log("Database connected successfully.");
-        initial();
+        // initial();
     })
     .catch((err) => {
         console.log("Failed to connect to the database.", err);
@@ -57,34 +57,34 @@ app.listen(process.env.PORT, () => {
     console.log(`Server is successfully running on ${process.env.PORT}`);
 });
 
-const Role = db.role;
+// const Role = db.role;
 
-function initial() {
-    Role.estimatedDocumentCount((err, count) => {
-        if (!err && count === 0) {
-            new Role({
-                name: "user",
-            }).save((err) => {
-                err
-                    ? console.log("Error occured: ", err)
-                    : console.log("added 'user' to roles collection");
-            });
+// function initial() {
+//     Role.estimatedDocumentCount((err, count) => {
+//         if (!err && count === 0) {
+//             new Role({
+//                 name: "user",
+//             }).save((err) => {
+//                 err
+//                     ? console.log("Error occured: ", err)
+//                     : console.log("added 'user' to roles collection");
+//             });
 
-            new Role({
-                name: "moderator",
-            }).save((err) => {
-                err
-                    ? console.log("Error occured: ", err)
-                    : console.log("added 'moderator' to roles collection");
-            });
+//             new Role({
+//                 name: "moderator",
+//             }).save((err) => {
+//                 err
+//                     ? console.log("Error occured: ", err)
+//                     : console.log("added 'moderator' to roles collection");
+//             });
 
-            new Role({
-                name: "admin",
-            }).save((err) => {
-                err
-                    ? console.log("Error occured: ", err)
-                    : console.log("added 'admin' to roles collection");
-            });
-        }
-    });
-}
+//             new Role({
+//                 name: "admin",
+//             }).save((err) => {
+//                 err
+//                     ? console.log("Error occured: ", err)
+//                     : console.log("added 'admin' to roles collection");
+//             });
+//         }
+//     });
+// }
--- backend/src/app/routes/donorRequest.route.js
+++ backend/src/app/routes/donorRequest.route.js
@@ -4,17 +4,18 @@ const donorRequest = require("../controllers/donorRequest.controller");
 
 const router = Router();
 
-router.post("/", [authJwt.verifyToken], donorRequest.create);
+// router.post("/", [authJwt.verifyToken], donorRequest.create);
+router.post("/", donorRequest.create);
 router.get("/", donorRequest.readAll);
 router.get("/:id", donorRequest.readOne);
 router.patch(
     "/:id",
-    [authJwt.verifyToken, donorReqPerm.canUpdateDelete],
+    // [authJwt.verifyToken, donorReqPerm.canUpdateDelete],
     donorRequest.update
 );
 router.delete(
     "/:id",
-    [authJwt.verifyToken, donorReqPerm.canUpdateDelete],
+    // [authJwt.verifyToken, donorReqPerm.canUpdateDelete],
     donorRequest.delete
 );
 
