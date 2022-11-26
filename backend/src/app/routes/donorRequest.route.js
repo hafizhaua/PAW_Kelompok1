68a368fab92cee172225d7f0088ea5c51d0bbf87
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2022-11-26 19:37:52 +0700

    feat: auth restriction on modifying donorreq

--- backend/src/app/routes/donorRequest.route.js
+++ backend/src/app/routes/donorRequest.route.js
@@ -4,18 +4,17 @@ const donorRequest = require("../controllers/donorRequest.controller");
 
 const router = Router();
 
-// router.post("/", [authJwt.verifyToken], donorRequest.create);
 router.post("/", donorRequest.create);
 router.get("/", donorRequest.readAll);
 router.get("/:id", donorRequest.readOne);
 router.patch(
     "/:id",
-    // [authJwt.verifyToken, donorReqPerm.canUpdateDelete],
+    [authJwt.verifyToken, authJwt.isAdmin],
     donorRequest.update
 );
 router.delete(
     "/:id",
-    // [authJwt.verifyToken, donorReqPerm.canUpdateDelete],
+    [authJwt.verifyToken, authJwt.isAdmin],
     donorRequest.delete
 );
 
