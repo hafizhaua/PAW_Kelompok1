cd4a0dc0b7793c258218320b9a2103d89e05a016
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2022-12-02 12:37:16 +0700

    fix: enable all cors

--- backend/app.js
+++ backend/app.js
@@ -8,14 +8,14 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 
-const corsOptions = {
-    origin: "https://bloodio.vercel.app/",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: "https://bloodio.vercel.app",
+//     credentials: true, //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
 // Routes
 const requestRoute = require("./src/app/routes/donorRequest.route");
